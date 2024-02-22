b47b0103d48e35a63693ea6c787c65a81186ab7c
Matheus Diôgo <matheusdiogoeng@gmail.com>
2024-02-22 16:50:18 -0300

    Delete teste.py

--- teste.py
+++
@@ -1,6 +0,0 @@
-import requests
-import pandas as pd
-
-url = 'https://servicodados.ibge.gov.br/api/v3/agregados/9522/periodos/2022/variaveis/93|6318|614?localidades=N6[1100023,1100031]'
-dados = requests.get(url=url).json()
-print(dados)
